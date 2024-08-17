5b1495350d5d7cc5086a211d57092cda1ba8fd60
Huzaifa <HuzaifaIslamRakib@gmail.com>
2024-08-17 20:23:29 +0600

    added all files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        bebas: ["Bebas Neue", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
